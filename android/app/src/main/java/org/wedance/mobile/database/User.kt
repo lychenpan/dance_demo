4c2254d28db83f939b26e2933deca7e287f4660c
lychenpan <lychenpan2@gmail.com>
2020-08-01 13:03:15 +0800

    database some code

--- android/app/src/main/java/org/wedance/mobile/database/User.kt
+++ android/app/src/main/java/org/wedance/mobile/database/User.kt
@@ -1,12 +1,28 @@
 package org.wedance.mobile.database
 
 import androidx.room.ColumnInfo
+import androidx.room.Embedded
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
 @Entity
 data class User(
     @PrimaryKey val uid: Int,
-    @ColumnInfo(name = "first_name") val firstName: String?,
-    @ColumnInfo(name = "last_name") val lastName: String?
+    val videoid: Int
+)
+
+data class Keypoints(
+    val x: String,
+    val y: String,
+    val score: String
+)
+//  x: "x1,x2,....x17"
+
+//a frame's estimation results
+data class vedio(
+    @Embedded val keypoints1: Keypoints,
+    @Embedded val keypoints2: Keypoints
+//    @Embedded val keypoints3: Keypoints
+//    @Embedded val keypoints4: Keypoints,
+//    @Embedded val keypoints5: Keypoints,
 )
